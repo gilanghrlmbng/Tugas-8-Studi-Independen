b7945cdc768b2dd5b6a7ca2330aea59541333f7a
gilanghrlmbng <gilanghrlmbng03@gmail.com>
2021-09-10 22:48:39 +0700

    revisi

--- app/src/main/java/com/e/recyclerviewcardgallery/MainActivity.java
+++ app/src/main/java/com/e/recyclerviewcardgallery/MainActivity.java
@@ -13,33 +13,33 @@ import android.widget.Toast;
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private TextView forgotpw;
-    private Button login;
+    private TextView login;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        forgotpw = findViewById(R.id.forgot);
-        login = findViewById(R.id.btn);
+        login = findViewById(R.id.login);
+        button = findViewById(R.id.btn);
 
-        forgotpw.setOnClickListener(this);
         login.setOnClickListener(this);
+        button.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        if (v == forgotpw) {
-            Toast.makeText(getApplicationContext(), "Anda lupa password", Toast.LENGTH_LONG).show();
-        } else if (v == login) {
-            Snackbar sbsukses = Snackbar.make(v.getRootView(), "Login Succeed", Snackbar.LENGTH_LONG);
+        if (v == login) {
+            Toast.makeText(getApplicationContext(), "Login", Toast.LENGTH_LONG).show();
+        } else if (v == button) {
+            Snackbar sbsukses = Snackbar.make(v.getRootView(), "Pendaftaran Sukses", Snackbar.LENGTH_LONG);
             sbsukses.show();
         }
 
     }
 
     public void openSnackbar(View view) {
-        Snackbar snackbar = Snackbar.make(view, "Ini adalah snackbar", Snackbar.LENGTH_LONG);
+        Snackbar snackbar = Snackbar.make(view, "Pendaftaran Sukses", Snackbar.LENGTH_LONG);
         snackbar.getView().setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.colorbg));
         snackbar.show();
     }
